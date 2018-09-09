1ac1b8e19aa5aedfa9d03ffbb806cae69eed44a3
Rajvir Rai <rsr1992@gmail.com>
2018-09-09 03:06:45 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,22 +1,36 @@
-def using_push(countries_in_western_africa, next_country)
-  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-  next_country = "Niger"
-  countries_in_western_africa.push(next_country)
+def using_push(arr, str)
+  arr.push(str)
 end
-
-def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
-  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-  new_neighborhood = "Brooklyn Heights"
-  neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
+ def using_unshift(arr, str)
+  arr.unshift(str)
 end
-
-def using_pop(great_hits_of_the_nineties)
-  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-  great_hits_of_the_nineties.pop
+ def using_pop(arr)
+  arr.pop
 end
-
-def pop_with_args(chars_in_game_of_thrones)
- chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
-  chars_in_game_of_thrones.pop(2)
+ def pop_with_args(arr)
+  arr.pop(2)
 end
-
+ def using_shift(arr)
+  arr.shift
+end
+ def shift_with_args(arr)
+  arr.shift(2)
+end
+ def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+ def using_insert(arr, str)
+  arr.insert(4, str)
+end
+ def using_uniq(arr)
+  arr.uniq
+end
+ def using_flatten(arr)
+  arr.flatten
+end
+ def using_delete(arr, str)
+  arr.delete(str)
+end
+ def using_delete_at(arr, int)
+  arr.delete_at(int)
+end
