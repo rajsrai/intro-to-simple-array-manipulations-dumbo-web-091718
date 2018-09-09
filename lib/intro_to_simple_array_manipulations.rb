2a1ee7fa13e3d840b5a4e617086909a2cdeac72a
Rajvir Rai <rsr1992@gmail.com>
2018-09-09 02:59:21 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,6 +10,8 @@ def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
   neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
 end
 
-def using_pop(great_hits_of_the_nineties)
+def using_pop(great_hits_of_the_nineties, deleted_string)
   great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-  
+  deleted_string = "Losing My Religion"
+  great_hits_of_the_nineties.pop(deleted_string)
+end
